54eee00eebdbec716e104aa7e40a765237cdeef7
Sean <iolupio@.gmail.com>
2023-05-06 23:02:07 +0800

    add index

--- web/app.py
+++ web/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/home")
+def home():
+    return render_template('index.html')
+    
